38cbc2e700d3da75fe07eee7ee3fe9107cd4152a
J B <jkbcm@users.noreply.github.com>
2018-05-30 18:25:16 +0100

    Update test file name

--- test/curl_extractTableToHTML.sh
+++ test/curl_extractTableToHTML.sh
@@ -3,6 +3,6 @@
 # Call one-shot service endpoint extractTableToHTML
 # Note: this is a long-running process 
 
-curl --form userWorkspace="user2" --form corpusName="corpus2" --form docName="doc2" --form "fileupload=@pdftestp5only.pdf" --form x0=17.5 --form y0=26 --form width=178.5 --form height=97.5 --form pageNumber=1 http://localhost:3000/api/extractTableToHTML
+curl --form userWorkspace="user2" --form corpusName="corpus2" --form docName="doc2" --form "fileupload=@testpdf.pdf" --form x0=17.5 --form y0=26 --form width=178.5 --form height=97.5 --form pageNumber=1 http://localhost:3000/api/extractTableToHTML
 
 
